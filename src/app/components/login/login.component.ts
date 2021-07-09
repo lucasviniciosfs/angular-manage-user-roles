06793fdea6c586d0b88613d8202bf2df157aa12b
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2021-07-09 16:19:02 -0300

    no message

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+  loading: boolean = false;
+  submitted: boolean = false;
+  error: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { 
+    if (this.authenticationService.userSubjectValue) {
+      this.router.navigate(['/']);  
+    }
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+    });
+  }
+  
+  public get f() {
+    return this.loginForm.controls;
+  }
+  
+  onSubmit(){
+    this.submitted = true;
+    
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService.login(this.f.username.value, this.f.password.value)
+    .pipe(first())
+    .subscribe({
+        next: () => {
+            // get return url from query parameters or default to home page
+            const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+            this.router.navigateByUrl(returnUrl);
+        },
+        error: error => {
+            this.error = error;
+            this.loading = false;
+        }
+    });
+  }  
+}
