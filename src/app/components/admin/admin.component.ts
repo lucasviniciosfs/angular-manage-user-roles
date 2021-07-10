c4c3eb0789a692fec2e98fa72f810408d1bf5213
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2021-07-10 17:07:43 -0300

    admin component

---
+++ src/app/components/admin/admin.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/user';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  loading = false;
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+      this.loading = true;
+      this.userService.getAll().pipe(first()).subscribe(users => {
+          this.loading = false;
+          this.users = users;
+      });
+  }
+}
